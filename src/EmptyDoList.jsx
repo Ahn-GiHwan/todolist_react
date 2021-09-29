2b0bcadc119909b0fa916e2834002d296cfb2442
Ahn-GiHwan <ahngh0113@naver.com>
2021-09-29 13:09:10 +0900

    feat: todolist가 비었을 때의 Component

---
+++ src/EmptyDoList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { EmptyDoList } from "./style/style";
+
+import { faGrinAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function EmptyDoListComponent() {
+  return (
+    <EmptyDoList>
+      <span>
+        할 일을 추가해 주세요 <FontAwesomeIcon icon={faGrinAlt} />
+      </span>
+    </EmptyDoList>
+  );
+}
+
+export default EmptyDoListComponent;
