ac109703679541e6f7cbff7c9c6f4a55a73df3e3
Ahn-GiHwan <ahngh0113@naver.com>
2021-09-29 13:01:04 +0900

    feat: React Container Component
    use context and useReducer
    isModal의 상태에 따라 Modal Component 출력

---
+++ src/Container.jsx
@@ -0,0 +1,21 @@
+import React, { useReducer } from "react";
+import Main from "./Main";
+import Modal from "./Modal";
+import StateContext, { initState, reducer } from "./State";
+
+import { Container } from "./style/style";
+
+const ContainerComponent = React.memo((props) => {
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  return (
+    <StateContext.Provider value={{ state, dispatch }}>
+      <Container t="0" l="0" b="0" r="0">
+        <Main />
+        {state.isModal && <Modal />}
+      </Container>
+    </StateContext.Provider>
+  );
+});
+
+export default ContainerComponent;
