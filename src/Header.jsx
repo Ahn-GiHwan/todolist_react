ef3219424c4d81d91d6de6255ac0f49bdc527c55
Ahn-GiHwan <ahngh0113@naver.com>
2021-10-12 21:45:45 +0900

    fix: code fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { Header, ListIcon, Title } from "./style/style.js";
-
 import { faThList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
