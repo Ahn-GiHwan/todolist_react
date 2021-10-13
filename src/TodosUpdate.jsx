1758e46528e0b85a2011fefe920ad1d5ccb3c10f
Ahn-GiHwan <ahngh0113@naver.com>
2021-10-13 15:44:49 +0900

    feat: todo update 유무를 결정하는 component 생성

---
+++ src/TodosUpdate.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import StateContext from "./State";
+import { Check, UpdateBox, UpdateContent } from "./style/style";
+
+const TodosUpdate = React.memo(() => {
+  const context = useContext(StateContext);
+
+  const { dispatch } = context;
+
+  const [check, setCheck] = useState(true);
+
+  useEffect(() => {
+    if (check) interval = setInterval(loopFunction, 10000);
+    else clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [check]);
+
+  let interval;
+
+  const loopFunction = () => {
+    axios({
+      method: "get",
+      url: "https://skytodo-express.herokuapp.com/todos",
+    }).then((res) => dispatch({ type: "updateTodos", todos: res.data }));
+  };
+
+  const onCheckInput = () => {
+    setCheck((state) => !state);
+  };
+
+  return (
+    <UpdateBox>
+      <Check checked={check} onChange={onCheckInput} />
+      <UpdateContent>10초마다 자동 업데이트</UpdateContent>
+    </UpdateBox>
+  );
+});
+
+export default TodosUpdate;
